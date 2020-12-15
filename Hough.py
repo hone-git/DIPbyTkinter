f63fb7930cdcd45cf119ebbe2bd2ed2cc6870ec0
hone-git <sundae4696@icloud.com>
2020-12-15 18:41:52 +0900

    add Hough

---
+++ Hough.py
@@ -0,0 +1,130 @@
+"""
+"""
+# ファイル関係
+import os
+from os.path import expanduser
+# 画像処理関係
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2
+from PIL import Image, ImageTk
+# GUI関係
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.filedialog
+
+imgs = []
+x = np.arange(256)
+old_x = None
+old_y = None
+
+dir = expanduser("~") + "/Pictures/dipImage"
+try:
+    os.chdir(dir)
+except FileNotFoundError:
+    os.mkdir(dir)
+    os.chdir(dir)
+
+def convert(img_bgr):
+    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+    img_pil = Image.fromarray(img_rgb)
+    img_tk = ImageTk.PhotoImage(img_pil)
+    imgs.append(img_tk)
+    return img_tk
+
+
+def NegPosInversion(img_src):
+    y = 255 - x
+    y = np.clip(y, 0, 255).astype(np.uint8)
+    img_dst = cv2.LUT(img_src, y)
+    return img_dst
+
+def Hough(img_gray, img_bgr):
+    img_gray = NegPosInversion(img_gray)
+    threshold = scl_threshold.get()
+    mLL = scl_minLineLength.get()
+    mLG = scl_maxLineGap.get()
+    lines = cv2.HoughLinesP(img_gray, rho=1, theta=np.pi/360, threshold=threshold, minLineLength=mLL, maxLineGap=mLG)
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        img = cv2.line(img_bgr, (x1,y1), (x2,y2), (0,0,255), 3)
+    return img_bgr
+
+def HoughCircle(img_gray, img_bgr):
+    img_gray = NegPosInversion(img_gray)
+    circles = cv2.HoughCircles(img_gray,cv2.HOUGH_GRADIENT,1,20, param1=50,param2=30,minRadius=40,maxRadius=0)
+    circles = np.uint16(np.around(circles))
+    for i in circles[0,:]:
+        cv2.circle(img_bgr,(i[0],i[1]),i[2],(0,255,0),2)
+        cv2.circle(img_bgr,(i[0],i[1]),2,(0,0,255),3)
+    return img_bgr
+
+def start(event):
+    global old_x, old_y
+    if old_x and old_y:
+        cnv.create_line(old_x, old_y, event.x, event.y, width=3)
+    old_x = event.x
+    old_y = event.y
+
+
+def stop(event):
+    global old_x, old_y
+    old_x = None
+    old_y = None
+    cnv.postscript(file="test.eps")
+    im = Image.open("test.eps")
+    im.save('test.png', "png")
+    img_origin = cv2.imread("test.png")
+    img_gray = cv2.cvtColor(img_origin, cv2.COLOR_BGR2GRAY)
+    img_hough = HoughCircle(img_gray, img_origin)
+    img_resized = cv2.resize(img_hough, (256, 256))
+    cnv_hough.create_image(0, 0, image=convert(img_resized), anchor="nw")
+
+# --------------------------------------------------
+# GUI
+
+root = tk.Tk()
+root.title("DigitalImageProcessing GUI")
+
+cnv = tk.Canvas(root, bg="white", width=512, height=512)
+cnv.grid(row=0, column=0)
+cnv.bind('<B1-Motion>', start)
+cnv.bind('<ButtonRelease-1>', stop)
+
+frm = tk.LabelFrame(root, text="Hough Transform")
+frm.grid(row=0, column=1)
+
+cnv_hough = tk.Canvas(frm, width=256, height=256)
+cnv_hough.grid(row=0, column=0)
+
+frame_parametor = tk.LabelFrame(frm, text="Parameter", width=256, height=256)
+frame_parametor.grid(row=1, column=0)
+frame_parametor.grid_propagate(False)
+
+lbl_threshold = tk.Label(frame_parametor, text="threshold")
+lbl_threshold.grid(row=0, column=0)
+
+scl_threshold = tk.Scale(frame_parametor, orient="horizontal", from_=0, to=160, resolution=10)
+scl_threshold.grid(row=0, column=1, sticky=tk.NSEW)
+scl_threshold.set(80)
+
+lbl_minLineLength = tk.Label(frame_parametor, text="minLineLength")
+lbl_minLineLength.grid(row=1, column=0)
+
+scl_minLineLength = tk.Scale(frame_parametor, orient="horizontal", from_=0, to=800, resolution=10)
+scl_minLineLength.grid(row=1, column=1, sticky=tk.NSEW)
+scl_minLineLength.set(80)
+
+lbl_maxLineGap = tk.Label(frame_parametor, text="maxLineGap")
+lbl_maxLineGap.grid(row=2, column=0)
+
+scl_maxLineGap = tk.Scale(frame_parametor, orient="horizontal", from_=0, to=20)
+scl_maxLineGap.grid(row=2, column=1, sticky=tk.NSEW)
+scl_maxLineGap.set(5)
+
+for i, child in enumerate(frame_parametor.winfo_children()):
+    frame_parametor.grid_rowconfigure(i, weight=1)
+
+frame_parametor.grid_columnconfigure(1, weight=1)
+
+root.mainloop()
